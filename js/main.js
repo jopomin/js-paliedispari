f708603f27cfaf85bd948ffca5c8d09bf9da82db
Giovanni <giovannipor23@gmail.com>
2021-02-28 22:20:09 +0100

    Palindroma (con funzione)
    Codice ripulito - Logica funziona, esercizio no

--- js/main.js
+++ js/main.js
@@ -1,52 +1,6 @@
 //inserire una parola
 var parola = prompt("Inserisci una parola lunga almeno tre lettere");
 
-/* var parola = inserimento.toUpperCase();
-
-console.log(parola);
-console.log(parola.length);
-
-//trasformarla in array
-var parolArray = Array.from(parola);
-
-console.log(parolArray);
-console.log(parolArray.length);
-
-//dividere la lunghezza dell'array a metà
-var meta = Math.floor(parolArray.length / 2);
-console.log(meta);
-
-//creare due array vuoti per contenere le due metà delle parole
-var primaMeta = [];
-var secondaMeta = [];
-
-//riempire secondo array con prima metà del primo
-for (i = 0; i < meta; i++) {
-    primaMeta[i] = parolArray[i];
-    console.log(primaMeta[i]);
-}
-
-console.log(primaMeta);
-
-//riempire terzo array con seconda metà invertita del primo
-for (x = 0; x < meta; x++) {
-    secondaMeta[x] = parolArray[parolArray.length-(x+1)];
-    console.log(secondaMeta[x]);
-}
-
-console.log(secondaMeta);
-
-
-//confrontare i due array. Se uguali, la parola è palindroma, se diversi, non lo è.
-
-if (primaMeta == secondaMeta) {
-    console.log("La parola è palindroma");
-    alert("La parola è palindroma");
-} else {
-    console.log("La parola non è palindroma");
-    alert("La parola non è palindroma");
-} */
-
 function pal (word) {
     console.log(word);
     var stringa = word.toUpperCase();
@@ -70,10 +24,8 @@ function pal (word) {
     console.log(secondHalf);
     if (firstHalf == secondHalf) {
         return "La parola è palindroma"
-        console.log("La parola è palindroma");
     } else {
         return "La parola non è palindroma"
-        console.log("La parola non è palindroma");
     }
     
 
